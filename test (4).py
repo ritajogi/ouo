286d7dc594eb3d898e2a0361387c650fbe603f32
ritajogi <122925250+ritajogi@users.noreply.github.com>
2023-04-09 20:13:13 +0500

    =

---
+++ test (4).py
@@ -0,0 +1,63 @@
+import re
+import time
+import requests
+import cloudscraper
+from bs4 import BeautifulSoup
+from urllib.parse import urlparse
+
+print("Python is Amazing! Lets Continue Angel.")
+
+
+url = "https://ouo.press/OTxBm2"
+
+# ----- : By ANGEL
+# ------------------------------------------------------------------------------------- #
+# RECAPTCHA v3 BYPASS
+def RecaptchaV3(ANCHOR_URL="https://www.google.com/recaptcha/api2/anchor?ar=1&k=6Lcr1ncUAAAAAH3cghg6cOTPGARa8adOf-y9zv2x&co=aHR0cHM6Ly9vdW8uaW86NDQz&hl=en&v=1B_yv3CBEV10KtI2HJ6eEXhJ&size=invisible&cb=4xnsug1vufyr"):
+    url_base = 'https://www.google.com/recaptcha/'
+    post_data = "v={}&reason=q&c={}&k={}&co={}"
+    client = requests.Session()
+    client.headers.update({
+        'content-type': 'application/x-www-form-urlencoded'
+    })
+    matches = re.findall('([api2|enterprise]+)\/anchor\?(.*)', ANCHOR_URL)[0]
+    url_base += matches[0]+'/'
+    params = matches[1]
+    res = client.get(url_base+'anchor', params=params)
+    token = re.findall(r'"recaptcha-token" value="(.*?)"', res.text)[0]
+    params = dict(pair.split('=') for pair in params.split('&'))
+    post_data = post_data.format(params["v"], token, params["k"], params["co"])
+    res = client.post(url_base+'reload', params=f'k={params["k"]}', data=post_data)
+    answer = re.findall(r'"rresp","(.*?)"', res.text)[0]
+    return answer
+
+
+# code
+def ouo(url):
+    client = requests.Session()
+    tempurl = url.replace("ouo.press", "ouo.io")
+    p = urlparse(tempurl)
+    id = tempurl.split('/')[-1]
+
+    res = client.get(tempurl)
+    next_url = f"{p.scheme}://{p.hostname}/go/{id}"
+
+    for _ in range(2):
+        if res.headers.get('Location'):
+            break
+        bs4 = BeautifulSoup(res.content, 'lxml')
+        inputs = bs4.form.findAll("input", {"name": re.compile(r"token$")})
+        data = { input.get('name'): input.get('value') for input in inputs }
+
+        ans = RecaptchaV3()
+        data['x-token'] = ans
+        h = {
+            'content-type': 'application/x-www-form-urlencoded'
+        }
+        res = client.post(next_url, data=data, headers=h, allow_redirects=False)
+        next_url = f"{p.scheme}://{p.hostname}/xreallcygo/{id}"
+
+    return res.headers.get('Location')
+
+# ------------------------------------------------------------------------------------- #
+print("Here is Your Bypassed Url:", ouo(url))
